c17f69570a0e2000fcd5129f19c02f7c8ddab03e
ankurdotio <prajapatiankur8349@gmail.com>
2026-09-09 00:06:49 +0530

    feat: implement access and refresh token authentication
    - add package.json with necessary dependencies and scripts
    - create app.js to set up Express application and middleware
    - configure environment variables in config.js
    - establish MongoDB connection in db.js
    - define user model with Mongoose in user.model.js
    - implement authentication routes in auth.routes.js including registration, fetching user info, and token refresh
    - set up server entry point in server.js
    - add utility functions for token generation and verification in auth.js

--- 0003-access-refresh-token/src/models/user.model.js
+++ 0003-access-refresh-token/src/models/user.model.js
@@ -5,19 +5,23 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        minLength: 3,
-        maxLength: 50
+        minLength: [ 3, "Name must be at least 3 characters long" ],
+        maxLength: [ 50, "Name must be at most 50 characters long" ]
     },
     email: {
         type: String,
         required: true,
+        unique: true,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
     },
     passwordHash: {
         type: String,
         required: true
+    },
+    refreshToken: {
+        type: String,
     }
 })
-
 
 const userModel = mongoose.model("users", userSchema)
 
