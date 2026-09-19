73dd52c2d441a45f104c10884fb23b93112c9678
ankurdotio <prajapatiankur8349@gmail.com>
2026-09-19 22:04:14 +0530

    feat: add authentication with JWT, including login and refresh token functionality

--- 0006-snitch/server/src/routes/auth.routes.js
+++ 0006-snitch/server/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
-import { registerValidator } from "../validators/auth.validator.js"
-import { register } from "../controller/auth.controller.js"
+import { registerValidator, loginValidator } from "../validators/auth.validator.js"
+import { register, login, refresh, getMe } from "../controller/auth.controller.js"
+import { authenticate } from "../middlewares/auth.middleware.js"
 
 
 
@@ -14,6 +15,27 @@ const router = Router()
  * @response res.status = 201 (if successful)
  */
 router.post("/register", registerValidator, register)
+
+
+/**
+ * @POST /api/auth/login
+ * @param req
+ * @param req.body = {email,password}
+ * res.status = 200
+ */
+router.post("/login", loginValidator, login)
+
+
+/**
+ * @POST /api/auth/refresh
+ */
+router.post('/refresh', refresh)
+
+
+/**
+ * @GET /api/auth/me
+ */
+router.get("/me", authenticate, getMe)
 
 
 export default router
--- 0006-snitch/server/src/utils/auth.utils.js
+++ 0006-snitch/server/src/utils/auth.utils.js
@@ -10,11 +10,19 @@ export function createAccessToken({ userId, role }) {
     return accessToken
 }
 
+export function readAccessToken(accessToken) {
+    return jwt.verify(accessToken, config.ACCESS_TOKEN_SECRET)
+}
+
 
 export function createRefreshToken({ userId, role }) {
     const refreshToken = jwt.sign({
         userId, role
     }, config.REFRESH_TOKEN_SECRET, { expiresIn: "7Days" })
-    
+
     return refreshToken
+}
+
+export function readRefreshToken(refreshToken) {
+    return jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET)
 }
