6a8335369fe1ec088f660bde4bfdab74f125a959
ankurdotio <prajapatiankur8349@gmail.com>
2026-09-23 23:31:39 +0530

    feat: add multer for file uploads and implement product creation endpoint
    - Added multer as a dependency for handling multipart/form-data.
    - Updated package.json and package-lock.json to include multer.
    - Created a new product controller with a createProduct function to handle product creation.
    - Implemented authentication middleware to protect the product creation route.
    - Added role verification to ensure only sellers can create products.
    - Enhanced the products route to handle file uploads and parse complex request bodies.

---
+++ 0006-snitch/server/src/controller/product.controller.js
@@ -0,0 +1,14 @@
+import productModel from "../models/product.model.js"
+
+
+
+
+export async function createProduct(req, res) {
+
+    console.log(req.body)
+    console.log(req.files)
+
+    res.status(200).json({
+        message: "Dummy response"
+    })
+}
